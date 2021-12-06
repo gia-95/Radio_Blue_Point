3ade4c13769166b8a9d51e8632c958c0aa293353
gia-95 <gianluca.marini95@gmail.com>
2021-12-06 14:53:02 +0100

    'Streaming Page' , aggiunta carattere 'Loster' , titolo

---
+++ Radio_Blue_Point/Radio_Blue_Point/Views/StreamingPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Radio_Blue_Point.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StreamingPage : ContentPage
+    {
+        public StreamingPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
