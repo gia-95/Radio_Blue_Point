707c9534714a42cd0b58f0ec9b63b82beb2b79e8
gia-95 <gianluca.marini95@gmail.com>
2021-12-08 13:54:11 +0100

    Aggiunta 'Carousel View' (StreamingPage), 'StreamingPageViewModel',  'ProgramPage.c'

---
+++ Radio_Blue_Point/Radio_Blue_Point/ProgramPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Radio_Blue_Point
+{
+    public class ProgramPage
+    {
+
+        public int id { get; set;  }
+
+        public string orario { get; set; }
+
+        public string nomeProgramma { get; set; }
+
+        public string imgProgram { get; set; }
+    }
+}
---
+++ Radio_Blue_Point/Radio_Blue_Point/ViewModels/StreamingPageViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace Radio_Blue_Point.ViewModels
+{
+    public class StreamingPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private ObservableCollection<ProgramPage> Program;
+
+        public ObservableCollection<ProgramPage> program
+        {
+            get { return Program; }
+            set { Program = value;
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("program"));
+            }
+        }
+
+
+        public StreamingPageViewModel()
+        {
+
+            program = new ObservableCollection<ProgramPage>();
+            addProgram();
+
+        }
+
+        public void addProgram()
+        {
+            program.Add(new ProgramPage
+            {
+                id = 0,
+                orario = "12.00",
+                nomeProgramma = "I Soliti Idioti",
+                imgProgram = "https://images.pexels.com/photos/6966/abstract-music-rock-bw.jpg"
+            });
+
+            program.Add(new ProgramPage
+            {
+                id = 0,
+                orario = "14.00",
+                nomeProgramma = "L'Eredità",
+                imgProgram = "https://images.pexels.com/photos/1105666/pexels-photo-1105666.jpeg"
+            });
+
+            program.Add(new ProgramPage
+            {
+                id = 0,
+                orario = "16.00",
+                nomeProgramma = "Bella Ciao",
+                imgProgram = "https://images.pexels.com/photos/675960/mic-music-sound-singer-675960.jpeg"
+            });
+
+        }
+
+    }
+}
