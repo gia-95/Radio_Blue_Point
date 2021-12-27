ab3fa27859acc908cc4dd1d7626fd53554c7d5e5
gia-95 <gianluca.marini95@gmail.com>
2021-12-27 13:11:28 +0100

    CollectionView 'SerchingPage'

---
+++ Radio_Blue_Point/Radio_Blue_Point/SongPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Radio_Blue_Point
+{
+    public class SongPage
+    {
+
+        public int id { get; set; }
+        public string titolo { get; set; }
+
+        public string album { get; set; }
+
+        public string imageSong { get; set; }
+
+    }
+}
---
+++ Radio_Blue_Point/Radio_Blue_Point/Views/ProvaPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Radio_Blue_Point.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProvaPage : ContentPage
+    {
+        public ProvaPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
