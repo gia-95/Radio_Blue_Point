db355e7ecfedb831a35d84e6b10d990d498fd255
gia-95 <gianluca.marini95@gmail.com>
2022-02-18 21:58:52 +0100

    Aggiunta icona

--- Radio_Blue_Point/Radio_Blue_Point.Android/MainActivity.cs
+++ Radio_Blue_Point/Radio_Blue_Point.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace Radio_Blue_Point.Droid
 {
-    [Activity(Label = "Radio_Blue_Point", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Radio Blue Point", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
 
